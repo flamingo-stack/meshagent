07c1f54dddb6e4c59c861d4f51a2c1b85eeeed86
Denys <denys@flamingo.cx>
2026-01-27 14:41:13 +0000

    fix: updated openssl lib path

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
-// This file is auto-generated, any edits may be overwritten
-#define SOURCE_COMMIT_DATE "2026-Jan-19 16:28:49+0000"
-#define SOURCE_COMMIT_HASH "50c686c86d03b1077da09e1665dc6bf473458f95"
+// This file is auto-generated, any edits may be overwritten 
+#define SOURCE_COMMIT_DATE "2026-Jan-27 15:46:03+0200"  
+#define SOURCE_COMMIT_HASH "68ecab052a1ab78012c2e4fb93be2d5899c6e9f6"  
