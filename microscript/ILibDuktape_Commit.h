4cfb671065e9858658b3dfcb306f1ff7377d7e6c
denys-gif <denys@flamingo.cx>
2026-02-11 20:06:55 +0000

    fix: permission window

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
-// This file is auto-generated, any edits may be overwritten 
-#define SOURCE_COMMIT_DATE "2026-Feb-3 14:16:17+0000"  
-#define SOURCE_COMMIT_HASH "0a52e0650fc9787dfd33e2132a201494e84b39ae"  
+// This file is auto-generated, any edits may be overwritten
+#define SOURCE_COMMIT_DATE "2026-Feb-4 11:58:30+0000"
+#define SOURCE_COMMIT_HASH "6ef9343d7facf3ec49ee612b0338a2b9a63e785b"
