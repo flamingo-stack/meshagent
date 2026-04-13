6603bf4155752f61674eecb37db0f0fe81da9044
denys-gif <denys@flamingo.cx>
2026-04-13 20:01:28 +0100

    feat: logs rotation

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten
-#define SOURCE_COMMIT_DATE "2026-Mar-30 11:57:13+0100"
-#define SOURCE_COMMIT_HASH "02d480fa36865fa6b920886aaabb299280bc054d"
+#define SOURCE_COMMIT_DATE "2026-Apr-2 15:17:01+0100"
+#define SOURCE_COMMIT_HASH "4007975a8492ff2363ed76e7caf66bd81371b18d"
