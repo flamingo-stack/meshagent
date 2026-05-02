d71e624c6262c1cb448b04f70f0d67e0110907ee
denys-gif <denys@flamingo.cx>
2026-05-02 15:03:11 +0100

    fix: token update

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten
-#define SOURCE_COMMIT_DATE "2026-Apr-30 12:07:46+0100"
-#define SOURCE_COMMIT_HASH "f6834432cd0c68601752ad35970b8bd341cf784f"
+#define SOURCE_COMMIT_DATE "2026-Apr-30 16:58:33+0100"
+#define SOURCE_COMMIT_HASH "ddb3f78afda60bfb58989f2525e352bc456cb32b"
