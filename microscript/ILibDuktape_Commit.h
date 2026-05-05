c041d2f943c4f08382c53b78622e7866650f3a9b
denys-gif <denys@flamingo.cx>
2026-05-05 14:49:05 +0100

    chore: add certificate logs

--- microscript/ILibDuktape_Commit.h
+++ microscript/ILibDuktape_Commit.h
@@ -1,3 +1,3 @@
 // This file is auto-generated, any edits may be overwritten
-#define SOURCE_COMMIT_DATE "2026-Apr-30 16:58:33+0100"
-#define SOURCE_COMMIT_HASH "ddb3f78afda60bfb58989f2525e352bc456cb32b"
+#define SOURCE_COMMIT_DATE "2026-May-2 15:03:11+0100"
+#define SOURCE_COMMIT_HASH "d71e624c6262c1cb448b04f70f0d67e0110907ee"
